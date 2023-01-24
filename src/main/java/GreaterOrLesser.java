144dc2af2ee8eebd194dbc47a271aaf0f95d84de
onusra <onusra@angelo.edu>
2023-01-24 18:22:17 +0000

    Less or equal comparison

--- src/main/java/GreaterOrLesser.java
+++ src/main/java/GreaterOrLesser.java
@@ -9,7 +9,15 @@ public class GreaterOrLesser {
      * @return  If a is less than b, return the string "Lesser". if a is greater than b, return "Greater".
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
+    public static void main(String [] args){
+
+    }
     public String decide(int a, int b){
+        if(a < b){
+            return "Lesser";
+        }else if (a > b){
+            return "Greater";
+        }
         return "Equal";
     }
 }
